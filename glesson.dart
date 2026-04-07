9eeb90de32c684a282b4a2066858789b97d40b01
ikindracode <ikindracode@gmail.com>
2026-04-07 20:40:13 +0600

    generics

--- glesson.dart
+++ glesson.dart
@@ -1,4 +1,31 @@
-void main(){
-    print("hello");
-    print("bye bye");
+class Storage<T>{
+  T item;
+
+
+  Storage(this.item);
+
+  void showItem(){
+    print("На складе лежит $item");
+
+  }
 }
+
+class Worker{
+  String name = 'Huan';
+
+  @override
+  String toString() => "Worker $name"; 
+}
+
+void main(){
+  var textStorage = Storage<String>("Secret documents");
+  textStorage.showItem();
+  
+  var intStorage = Storage<int>(404);
+  intStorage.showItem();
+
+  var workerbrother = Storage<Worker>(Worker());
+  workerbrother.showItem();
+}
+
+
